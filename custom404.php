361466ea48f00d94ff3b2c19c39653154e2ed5c9
Arkopravo Saha <arkopravo1@hotmail.com>
2024-12-02 20:29:37 +0530

    added custom 404

---
+++ custom404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Not Found</title>
+    <link rel="stylesheet" href="style2.css?v=1.0">
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">Alpine Technologies</div>
+        <div class="nav-buttons">
+            <button onclick="alert('Button 1 clicked')">1</button>
+            <button onclick="alert('Button 2 clicked')">2</button>
+            <a href="logout.php">
+              <button>Logout</button>
+            </a>
+        </div>
+    </div>
+    <h1>Lost in the woods?</h1>
+    <br>
+    <p>Oops! The page you are looking for does not exist.</p>
+    <p><a href="/Configuration_Management/homepage.php">Return to Home</a></p>
+</body>
+</html>
