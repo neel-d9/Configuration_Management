158a666cddf923ac51d807d93e3ca2d1a2a1cc24
Arkopravo Saha <arkopravo1@hotmail.com>
2024-12-07 03:30:18 +0530

    continuing to appease my endless navbar obsession without doing any actual work

--- homepage.php
+++ homepage.php
@@ -32,13 +32,40 @@ $userRole = isset($_SESSION['role']) && isset($roleMapping[$_SESSION['role']])
 </head>
 <body>
     <div class="navbar">
-        <div class="logo">Alpine Technologies</div>
+        <div class="logo">
+            <a href="index.php">Alpine Technologies</a>
+        </div>
         <div class="nav-buttons">
-            <button onclick="alert('Button 1 clicked')">1</button>
-            <button onclick="alert('Button 2 clicked')">2</button>
-            <a href="logout.php">
-              <button>Logout</button>
-            </a>
+            <?php if ($userRole=="Configuration Manager"): ?>
+              <a href="managecr.php">
+                <button>Manage CR</button>
+              </a>
+            <?php endif; ?>
+            <?php if ($userRole=="Developer"): ?>
+              <a href="checkcr.php">
+                <button>Check assigned CR</button>
+              </a>
+            <?php endif; ?>
+            <?php if ($userRole=="Developer" || $userRole=="Customer Support"): ?>
+              <a href="raisecr.php">
+                <button>Raise New CR</button>
+              </a>
+            <?php endif; ?>
+
+            <div class="divider"></div>
+            
+            <?php if (!isset($_SESSION['username'])): ?>
+              <a href="index.php">
+                <button>Login</button>
+              </a>
+            <?php else: ?>
+              <div class="user-info">
+                <span>Signed in as <?php echo $_SESSION['username'] ?></span>
+                <a href="logout.php">
+                    <button class="logout-button">Logout</button>
+                </a>
+              </div>
+            <?php endif; ?>
         </div>
     </div>
     <div class="card">
